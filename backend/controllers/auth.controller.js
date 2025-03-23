15ae9610ea8f224953f65b734caa43b85bb690ca
J C Lisboa <lookjota@gmail.com>
2025-03-23 14:31:35 -0300

    coding login 1:9:30

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -25,7 +25,7 @@ const setCookies = (res, accessToken, refreshToken) => {
     sameSite: "strict", // prevent CSRF attack, cross-site request forgery attack
     maxAge: 15 * 60 * 1000, // 15 minutes
   })
-  res.cookie("accessToken", refreshToken, {
+  res.cookie("refreshToken", refreshToken, {
     httpOnly: true, // prevent XSS attacks, cross site scripting attack
     secure:process.env.NODE_ENV == "production",
     sameSite: "strict", // prevent CSRF attack, cross-site request forgery attack
@@ -50,21 +50,55 @@ export const signup = async (req, res) => {
     setCookies(res, accessToken, refreshToken)
 
     res.status(201).json({ 
-      user:{ 
         _id: user._id, 
         name: user.name,
         email: user.email,
         role: user.role,
-      },
-      message: "User created successfully" })
+      })
   } catch (error) {
     res.status(500).json({ message: error.message })
   }
 }
 
 export const login = async (req, res) => {
-  res.send("Login up route called")
+  try {
+    console.log("here runs the login")
+
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+    console.log("here runs the login2")
+
+    if (user && (await user.comparePassword(password))) {
+      const {accessToken, refreshToken} = generateTokens(user._id)
+      console.log("user is logged in")
+
+      await storeRefreshToken(user._id, refreshToken)
+      setCookies(res, accessToken, refreshToken)
+
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      })
+    } 
+  }  catch (error) {
+    console.log("Error in login controller", error.message)
+    res.status(401).json({ message: "Invalid credentials" })
+  }
 }
 export const logout = async (req, res) => {
-  res.send("Logout up route called")
+  try {
+    const refreshToken = req.cookies.refreshToken
+    if (refreshToken) {
+      const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+      await redis.del(`refresh_token:${decoded.userId}`)
+    }
+    res.clearCookie("accessToken")
+    res.clearCookie("refreshToken")
+    res.json({ message: "Logout successful" })
+  } catch (error) {
+    console.log("Error in logout controller", error.message)
+    res.status(500).json({ message: "Server error", error: error.message })
+  }
 }
