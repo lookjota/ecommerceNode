2fdbd3498e91c88c4889251a50da93ea5e678d9a
J C Lisboa <lookjota@gmail.com>
2025-03-24 16:38:23 -0300

    Product routes 1:48:12

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,61 @@
+import cloudinary from "../lib/cloudinary.js"
+import Product from "../models/product.model.js"
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}) //find all products
+    res.json({ products })
+  } catch(error) {
+    console.log("Error in getAllProducts controller", error.message)
+    res.status(500).json({ message: "Server error", error: error.message })
+  }
+}
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products")
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts))
+    }
+    // if not in redis, fetch from mongoDB
+    // .lean() is gonna return a plain javascript object instead of a mongodb docmuent
+    // which is good for performance
+    featuredProducts = await Product.find({isFeatured:true}).lean()
+
+    if(!featuredProducts) {
+      return res.status(404).json({ message: "No featured products found" })
+    }
+
+    // store in redis for future quick access
+
+    await redis.set("featured_products", JSON.stringify(featuredProducts))
+
+    res.json(featuredProducts)
+  } catch (error) {
+    console.log("Error in getFeaturedProducts controller", error.message)
+    res.status(500).json({ message: "Server error", error: error.message})
+  }
+}
+
+export const createProduct = async (req, res) => {
+  try {
+    const {name, description, price, image, category} = req.body
+
+    let cloudinaryResponse = null
+
+    if(image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image,{folder:"products"})
+    }
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+      category
+    })
+    res.status(201).json(product)
+  } catch (error) {
+    console.log("Error in createProduct controller", error.message)
+    res.status(500).json({ message: "Server error", error: error.message})
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
+
 import { connectDB } from "./lib/db.js"
-import cookieParser from "cookie-parser"
 
 dotenv.config()
 
@@ -14,6 +16,7 @@ app.use(express.json()) //allows you to parse the body of the request
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:" + PORT)
